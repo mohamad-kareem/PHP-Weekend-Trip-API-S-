c2b35336e2af0905bd298bd079efdcf9573708a0
mohamad-kareem <mohamadkareemeng@gmail.com>
2023-03-09 01:07:36 +0200

    fixing the palindrome

--- palindrome.php
+++ palindrome.php
@@ -1,10 +1,10 @@
 <?php
 $string=$_GET["input"];
 if($string==strrev($string)){
-    $response=["is palindrome"];
+    $response=["is palindrome"]="it is palindrome";
 }
 else{
-    $response=["it's not palindrome"];
+    $response=["is palindrome"]="it is not palindrome";
 }
 echo json_encode($response);
 
