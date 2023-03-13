e423b0ac16cfa6aef90ac88c81b0b6f9770ff978
mohamad-kareem <mohamadkareemeng@gmail.com>
2023-03-13 14:22:30 +0200

    adding the even/odd part

--- prime.php
+++ prime.php
@@ -1,18 +1,35 @@
 <?php
 
-$birthday=$_GET["input"];
-$present=date("y");
-$age=$present-$birthday;
-if $age<=1{
-    $response["prime"]="it not prime";
-}
-else{
-    for ($i = 2; $i <= sqrt($age); $i++) {
-    if ($age % $i == 0) {
-        $response["prime"]="it not prime";
+$birthday = $_GET["input"];
+$present = date("y");
+$age = $present - $birthday;
+
+if ($age <= 1) {
+    $response["prime"] = "it not prime";
+} 
+
+
+else {
+    if ($age % 2 == 0) {
+        $response["even"] = "age is even";
+    } else {
+        $response["even"] = "age is odd";
     }
-}
-$response["prime"]="age is prime";
+   
+    
+    
+    $is_prime = true;
+    for ($i = 2; $i <= sqrt($age); $i++) {
+        if ($age % $i == 0) {
+            $is_prime = false;
+            break;  }
+    }
+    
+    
+    if ($is_prime) {
+        $response["prime"] = "age is prime";
+    } else {
+        $response["prime"] = "it not prime"; }
 }
 echo json_encode($response);
 ?>
